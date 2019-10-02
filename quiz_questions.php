0ecdb68f29712492bb084d96277f8b6fcbd2815d
angelomatel <mateljohnangelo@gmail.com>
2019-10-02 22:11:16 +0800

    Added Questions

--- quiz_questions.php
+++ quiz_questions.php
@@ -11,7 +11,7 @@
 <div id="container">
     <div id="wrapper">
         <form action="quiz_results.php" method="POST">
-        <p class="big">#. Insert Question Here</p>
+        <p class="big">1. What do you use when commenting SINGLE lines in PHP?</p>
             <div>
                 <div id="child">AAAAAAAAAAAA</div>
                 <div id="child">BBBBBBBBBBBB</div>
@@ -20,18 +20,48 @@
                 <div id="child">CCCCCCCCCCCC</div>
                 <div id="child">DDDDDDDDDDDD</div>
             </div>
+        <br><br>
+        <p class="big">2. What is the data type that are a sequence of characters?</p>
         
         <br><br>
+        <p class="big">3. What is the data type that only consists of whole numbers?</p>
 
-        <p class="big">#. Insert Question Here</p>
-            <div>
-                <div id="child">AAAAAAAAAAAA</div>
-                <div id="child">BBBBBBBBBBBB</div>
-            </div>
-            <div>
-                <div id="child">CCCCCCCCCCCC</div>
-                <div id="child">DDDDDDDDDDDD</div>
-            </div>
+        <br><br>
+        <p class="big">4. What is the data type that only consists of numbers with decimal point?</p>
+
+        <br><br>
+        <p class="big">5. What is the data type that consists of multiple values?</p>
+
+        <br><br>
+        <p class="big">6. Which of the following functions should you use to get the length of the string       "Hello World"?</p>
+
+        <br><br>
+        <p class="big">7. Which of the following functions should you use to replace the word "Hello" in        "Hello World" with "Hi" to output "Hi World"?</p>
+
+        <br><br>
+        <p class="big">8. Which of the following functions should you use to reverse the string "Hello      World"?</p>
+
+        <br><br>
+        <p class="big">9. Which of the following functions should you use to get the number of words in         the string "Hello World"?</p>
+
+        <br><br>
+        <p class="big">10. How would you define a constant named STRING with the value of "Hello World"         that is case-insensitive using the define() function?</p>
+
+        <br><br>
+        <p class="big">11. Provided that $x is 2 and $y is 2.0, which of the following if() functions       would be true?</p>
+
+        <br><br>
+        <p class="big">12. Provided that $x is 2 and $y is 2.0, which if the following if() functions       would be false?</p>
+
+        <br><br>
+        <p class="big">13. Provided that $x and $y are true, and $z is false, which of the following if()       functions would be true?</p>
+
+        <br><br>
+        <p class="big">14. Provided that $x and $y are false, and $z is true, which of the following if()       functions would be true?</p>
+
+        <br><br>
+        <p class="big">15. What does PHP stand for?</p>
+
         </form>
     </div>
 </div>
