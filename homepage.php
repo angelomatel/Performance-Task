2c4747fd1cec1a89199638dfa7250ab12ef8c433
rom-beans <46665055+rom-beans@users.noreply.github.com>
2019-10-02 20:11:06 +0800

    Initial commit

---
+++ homepage.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <link rel="stylesheet" href="./assets/style.css">
+</head>
+
+<body>
+<div id="header">
+    <span class="title">The Quiz</span>
+</div>
+
+<div id="container">
+    <div id="wrapper">
+        <input type="button" value="Thing">
+    </div>
+</div>
+</body>
+
+</html>
