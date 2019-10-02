73ea5b360920084f612cc4ffabefe17c05686d08
angelomatel <mateljohnangelo@gmail.com>
2019-10-02 22:14:19 +0800

    Added Placeholders, Fixed Formatting

--- quiz_questions.php
+++ quiz_questions.php
@@ -13,55 +13,154 @@
         <form action="quiz_results.php" method="POST">
         <p class="big">1. What do you use when commenting SINGLE lines in PHP?</p>
             <div>
-                <div id="child">AAAAAAAAAAAA</div>
-                <div id="child">BBBBBBBBBBBB</div>
+                <div id="child"><input type="radio" id="A" name="1"></div>
+                <div id="child"><input type="radio" id="B" name="1"></div>
             </div>
             <div>
-                <div id="child">CCCCCCCCCCCC</div>
-                <div id="child">DDDDDDDDDDDD</div>
+                <div id="child"><input type="radio" id="C" name="1"></div>
+                <div id="child"><input type="radio" id="D" name="1"></div>
             </div>
         <br><br>
         <p class="big">2. What is the data type that are a sequence of characters?</p>
-        
+            <div>
+                <div id="child"><input type="radio" id="A" name="2"></div>
+                <div id="child"><input type="radio" id="B" name="2"></div>
+            </div>
+            <div>
+                <div id="child"><input type="radio" id="C" name="2"></div>
+                <div id="child"><input type="radio" id="D" name="2"></div>
+            </div>
         <br><br>
         <p class="big">3. What is the data type that only consists of whole numbers?</p>
-
+            <div>
+                <div id="child"><input type="radio" id="A" name="3"></div>
+                <div id="child"><input type="radio" id="B" name="3"></div>
+            </div>
+            <div>
+                <div id="child"><input type="radio" id="C" name="3"></div>
+                <div id="child"><input type="radio" id="D" name="3"></div>
+            </div>
         <br><br>
         <p class="big">4. What is the data type that only consists of numbers with decimal point?</p>
+            <div>
+                <div id="child"><input type="radio" id="A" name="4"></div>
+                <div id="child"><input type="radio" id="B" name="4"></div>
+            </div>
+            <div>
+                <div id="child"><input type="radio" id="C" name="4"></div>
+                <div id="child"><input type="radio" id="D" name="4"></div>
+            </div>
 
         <br><br>
         <p class="big">5. What is the data type that consists of multiple values?</p>
-
+            <div>
+                <div id="child"><input type="radio" id="A" name="5"></div>
+                <div id="child"><input type="radio" id="B" name="5"></div>
+            </div>
+            <div>
+                <div id="child"><input type="radio" id="C" name="5"></div>
+                <div id="child"><input type="radio" id="D" name="5"></div>
+            </div>
         <br><br>
-        <p class="big">6. Which of the following functions should you use to get the length of the string       "Hello World"?</p>
-
+        <p class="big">6. Which of the following functions should you use to get the length of the string "Hello World"?</p>
+            <div>
+                <div id="child"><input type="radio" id="A" name="6"></div>
+                <div id="child"><input type="radio" id="B" name="6"></div>
+            </div>
+            <div>
+                <div id="child"><input type="radio" id="C" name="6"></div>
+                <div id="child"><input type="radio" id="D" name="6"></div>
+            </div>
         <br><br>
-        <p class="big">7. Which of the following functions should you use to replace the word "Hello" in        "Hello World" with "Hi" to output "Hi World"?</p>
-
+        <p class="big">7. Which of the following functions should you use to replace the word "Hello" in "Hello World" with "Hi" to output "Hi World"?</p>
+            <div>
+                <div id="child"><input type="radio" id="A" name="7"></div>
+                <div id="child"><input type="radio" id="B" name="7"></div>
+            </div>
+            <div>
+                <div id="child"><input type="radio" id="C" name="7"></div>
+                <div id="child"><input type="radio" id="D" name="7"></div>
+            </div>
         <br><br>
-        <p class="big">8. Which of the following functions should you use to reverse the string "Hello      World"?</p>
-
+        <p class="big">8. Which of the following functions should you use to reverse the string "Hello World"?</p>
+            <div>
+                <div id="child"><input type="radio" id="A" name="8"></div>
+                <div id="child"><input type="radio" id="B" name="8"></div>
+            </div>
+            <div>
+                <div id="child"><input type="radio" id="C" name="8"></div>
+                <div id="child"><input type="radio" id="D" name="8"></div>
+            </div>
         <br><br>
-        <p class="big">9. Which of the following functions should you use to get the number of words in         the string "Hello World"?</p>
-
+        <p class="big">9. Which of the following functions should you use to get the number of words in the string "Hello World"?</p>
+            <div>
+                <div id="child"><input type="radio" id="A" name="9"></div>
+                <div id="child"><input type="radio" id="B" name="9"></div>
+            </div>
+            <div>
+                <div id="child"><input type="radio" id="C" name="9"></div>
+                <div id="child"><input type="radio" id="D" name="9"></div>
+            </div>
         <br><br>
-        <p class="big">10. How would you define a constant named STRING with the value of "Hello World"         that is case-insensitive using the define() function?</p>
-
+        <p class="big">10. How would you define a constant named STRING with the value of "Hello World" that is case-insensitive using the define() function?</p>
+            <div>
+                <div id="child"><input type="radio" id="A" name="10"></div>
+                <div id="child"><input type="radio" id="B" name="10"></div>
+            </div>
+            <div>
+                <div id="child"><input type="radio" id="C" name="10"></div>
+                <div id="child"><input type="radio" id="D" name="10"></div>
+            </div>
         <br><br>
-        <p class="big">11. Provided that $x is 2 and $y is 2.0, which of the following if() functions       would be true?</p>
-
+        <p class="big">11. Provided that $x is 2 and $y is 2.0, which of the following if() functions would be true?</p>
+            <div>
+                <div id="child"><input type="radio" id="A" name="11"></div>
+                <div id="child"><input type="radio" id="B" name="11"></div>
+            </div>
+            <div>
+                <div id="child"><input type="radio" id="C" name="11"></div>
+                <div id="child"><input type="radio" id="D" name="11"></div>
+            </div>
         <br><br>
-        <p class="big">12. Provided that $x is 2 and $y is 2.0, which if the following if() functions       would be false?</p>
-
+        <p class="big">12. Provided that $x is 2 and $y is 2.0, which if the following if() functions would be false?</p>
+            <div>
+                <div id="child"><input type="radio" id="A" name="12"></div>
+                <div id="child"><input type="radio" id="B" name="12"></div>
+            </div>
+            <div>
+                <div id="child"><input type="radio" id="C" name="12"></div>
+                <div id="child"><input type="radio" id="D" name="12"></div>
+            </div>
         <br><br>
-        <p class="big">13. Provided that $x and $y are true, and $z is false, which of the following if()       functions would be true?</p>
-
+        <p class="big">13. Provided that $x and $y are true, and $z is false, which of the following if() functions would be true?</p>
+            <div>
+                <div id="child"><input type="radio" id="A" name="13"></div>
+                <div id="child"><input type="radio" id="B" name="13"></div>
+            </div>
+            <div>
+                <div id="child"><input type="radio" id="C" name="13"></div>
+                <div id="child"><input type="radio" id="D" name="13"></div>
+            </div>
         <br><br>
-        <p class="big">14. Provided that $x and $y are false, and $z is true, which of the following if()       functions would be true?</p>
-
+        <p class="big">14. Provided that $x and $y are false, and $z is true, which of the following if() functions would be true?</p>
+            <div>
+                <div id="child"><input type="radio" id="A" name="14"></div>
+                <div id="child"><input type="radio" id="B" name="14"></div>
+            </div>
+            <div>
+                <div id="child"><input type="radio" id="C" name="14"></div>
+                <div id="child"><input type="radio" id="D" name="14"></div>
+            </div>
         <br><br>
         <p class="big">15. What does PHP stand for?</p>
-
+            <div>
+                <div id="child"><input type="radio" id="A" name="15"></div>
+                <div id="child"><input type="radio" id="B" name="15"></div>
+            </div>
+            <div>
+                <div id="child"><input type="radio" id="C" name="15"></div>
+                <div id="child"><input type="radio" id="D" name="15"></div>
+            </div>
         </form>
     </div>
 </div>
